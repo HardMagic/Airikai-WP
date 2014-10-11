27619cb7ca2a061ade340b9245e7aacde2571499
Matt Hackney <matt@themov.org>
2014-10-11 23:45:31 +0000

    Course Video Add

--- course-masonry-portfolio.php
+++ course-masonry-portfolio.php
@@ -13,7 +13,7 @@
 	$data = get_post_meta( $post->ID, 'portfolio_options', true );
 	if ( get_post_meta( $post->ID, 'course_video_url', true ) ){
 	$video_src = get_post_meta( $post->ID, 'course_video_url', true );
-	$video_html = $content .= wp_oembed_get( $video_src);
+	$video_html = wp_oembed_get( $video_src);
 	}
 	else
 	$video_html = '';

145859d51b6373c4c840a7da450bc415762d4992
Matt Hackney <matt@themov.org>
2014-10-11 22:34:42 +0000

    Course Masonry Update

--- course-masonry-portfolio.php
+++ course-masonry-portfolio.php
@@ -3,7 +3,7 @@
 ?>
 <?php
 	$taxonomy = 'portfolio-category';
-	$category_class = 'course';
+	$category_class = ' course';
 	$term_ids = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' =>'ids' ));
 	foreach( $term_ids as $term_id){
 		$term = get_term( $term_id, $taxonomy );

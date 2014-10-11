5d06f40c8f6111cf3d4a8f934e1975109cdf0f9a
Matt Hackney <matt@themov.org>
2014-10-11 20:41:10 +0000

    update image 2

--- course-masonry-portfolio.php
+++ course-masonry-portfolio.php
@@ -37,12 +37,12 @@ HEREDOC;
 	<div class="article">
 		<div class="img-holder n-s ro <?php echo $p_type ?>">
 			<a href="<?php the_permalink() ?>" data-img="<?php echo $thumb['b_href'] ?>" title="<?php echo $thumb['caption']; ?>">
-			<img <?php echo get_post_meta( $course_id, 'featured_url', true ); ?> />
+			<img src="<?php echo get_post_meta( $course_id, 'featured_url', true ); ?>" />
 			</a>
 			
 			<?php echo $vid_container ?>
 		</div>
-		<h4 class="entry-title _cf"><a href="<?php the_permalink() ?>"><?php the_title() ?>sss</a></h4>
+		<h4 class="entry-title _cf"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
 		<?php the_excerpt() ?>
 		<?php if( current_user_can('edit_posts')): // edit link?>
 			<a href="<?php echo get_edit_post_link($post->ID) ?>" class="button">

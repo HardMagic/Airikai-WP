5f3a563a25d0d8f3f73ab62eca813dc0ab380828
Matt Hackney <matt@themov.org>
2014-10-13 23:34:50 +0000

    Certifications addition

--- certification-masonry.php
+++ certification-masonry.php
@@ -1,87 +1,87 @@
-<?php 
-/* Template Name: Portfolio - Masonry - Certification*/
+<?php
+$t_flag = has_post_thumbnail($post->ID);
 ?>
-<?php get_header() ?>
-<div id="bg">
-	
-	<?php get_template_part('mobile-menu') ?>
-	<div id="top_bg"></div>
-	<div id="holder">
-		<?php get_template_part('aside') ?>
-		<div id="content">
-			<?php if( post_password_required() ): ?>
+<div class="article_box">
+	<div class="article_t">
+		<?php if( comments_open() && !post_password_required() ): // comments ?>
+			<a href="<?php comments_link() ?>" class="ico_link comments-a<?php echo $t_flag?'':' grey' ?>"><?php echo get_comments_number($post->ID) ?>
+			</a>
+		<?php endif ?>
+	</div>
+	<div class="article">
+		<?php if( $t_flag && !post_password_required() ): // post featuredimage ?>
+			<div class="img-holder n-s">
+				<?php
+				$args = array(	'post_id'	=>$post->ID,
+								'width'		=>240,
+								'upscale'	=>true
+								);
+				$thumb = dt_get_thumbnail( $args );
+				?>
+				<a href="<?php the_permalink() ?>" title="<?php echo $thumb['caption'] ?>" data-img="<?php echo $thumb['b_href'] ?>">	
+					<img <?php echo $thumb['size'][3] ?> src="<?php echo $thumb['t_href'] ?>" alt="<?php echo $thumb['alt'] ?>"/>
+				</a>
+			</div>
+		<?php endif ?>
+		<h4 class="entry-title _cf"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
+		<?php 
+		the_excerpt();
+		wp_link_pages();
+		?>
+		<?php if( current_user_can('edit_posts')): // edit link?>
+			<a href="<?php echo get_edit_post_link($post->ID) ?>" class="button">
+				<span class="but-r"><span><i class="detail"></i><?php echo __( 'Edit', LANGUAGE_ZONE ) ?></span></span>
+			</a>
+		<?php endif ?>
+		<a href="<?php the_permalink() ?>" class="button"><span class="but-r"><span><i class="detail"></i><?php _e( 'Details', LANGUAGE_ZONE) ?></span></span></a>       
+		
+		<?php if( !post_password_required() ): ?>
+		<div class="meta">
+			<div class="ico-l d">   
+				<a href="#" class="ico_link date"></a>
+				<div class="info-block">
+					<span class="grey"><?php _e( 'Published on:', LANGUAGE_ZONE ) ?></span><br/>
+					<a href="<?php the_permalink() ?>"><?php the_time(get_option('date_format'). ' '. get_option('time_format') ) ?></a>
+				</div>
+			</div>           
 			
-			<div class="article_box p">
-				<div class="article_t"></div>
-				<div class="article b">
-					<?php echo get_the_password_form(); ?>
-				</div><!-- .article b end -->
-				<div class="article_b"></div>
+			<div class="ico-l">
+				<span class="ico_link author"></span>
+				<div class="info-block">
+					<span class="grey"><?php echo __( 'Author:', LANGUAGE_ZONE ) ?></span><br />                 
+					<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>">
+						<?php echo get_the_author() ?>
+					</a>
+				</div>
 			</div>
 			
-			<?php else: ?>
-			
-			<?php
-			$port_terms = get_post_meta( $post->ID, 'show_portf', true );
-			$args = array( 'post_type'	=>'post' );
-			
-
-/*			CATEGORIES FOR COURSES - COMING SOON
-			http://premium.wpmudev.org/forums/topic/feature-requests-for-coursepress-pro
-		
-
-			if( isset($port_terms['number_portf']) && $port_terms['number_portf'] ) {
-				$args['posts_per_page'] = $port_terms['number_portf'];
-				unset($port_terms['number_portf']);
-			}
-
-			
-			if( $port_terms ) {
-				$args['tax_query'] = array(	
-											array(
-												'taxonomy'=>'portfolio-category',
-												'field'=>'id',
-												'terms'=>current( $port_terms ),
-												'operator' => ( 'only' == key($port_terms) )?'IN':'NOT IN',
-											)
-										);
-			}
-*/
-
-	     
-			$temp = $wp_query;
-			$wp_query = new Wp_Query( $args );
-/*
-			RELATED TO CATEGORIES 
-			if( $wp_query->have_posts() ): 
-				echo dt_portf_tax_list(
-					array(
-						'a_class'	=>'button big filter',
-						'c_class'	=>'filter-p filters',
-						'tax'		=>$port_terms,
-						'ajax'		=>true
-					)
-				);
-*/
+			<?php // category
+			 $categories = get_the_category_list( __( ', ', 'dt' ) );
+			 if( $categories ):
 			?>
-
-	
-				<div id="multicol" class="portfolio_massonry">
-					<?php while( $wp_query->have_posts() ): $wp_query->the_post(); ?>
-						<?php get_template_part('certification-masonry', 'portfolio'); ?>
-					<?php endwhile ?>
+				<div class="ico-l">
+					<span class="ico_link categories"></span>
+					<div class="info-block">
+						<span class="grey"><?php echo __( 'Categories:', 'dt' ) ?></span><br />					
+						<?php echo $categories ?>
+					</div>
 				</div>
-				<div id="nav-above" class="navigation portfolio _m">
-					<?php
-					if( function_exists('wp_pagenavi') ) wp_pagenavi();
-					else wp_link_pages();
-					?>
+			<?php endif ?>
+			
+			<?php //tags
+			 $tags = get_the_tag_list( '', __( ', ', 'dt' ) );
+			 if( $tags ):			
+			?>
+				<div class="ico-l">
+					<span class="ico_link tags"></span>
+					<div class="info-block">
+						<span class="grey"><?php echo __( 'Tags:', 'dt' ) ?></span><br />                 
+						<?php echo $tags ?>
+					</div>
 				</div>
-			<?php // else:?>
-			<?php // endif ?>
-			<?php $wp_query = $temp; ?>
-			<?php endif;// password protectection ?>
-		</div>
-	</div>
+			<?php endif ?>
+		</div><!-- meta end -->
+		<?php endif; ?>
+	</div><!-- .article end -->
+	<div class="article_footer_b"></div>
 </div>
-<?php get_footer() ?>

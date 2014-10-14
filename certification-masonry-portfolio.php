290e2d0527b2af753c99ace052628d64b74de8cb
Matt Hackney <matt@themov.org>
2014-10-14 02:05:31 +0000

    Certifications Filter

--- certification-masonry-portfolio.php
+++ certification-masonry-portfolio.php
@@ -28,6 +28,7 @@
 			$args = array(
 				'post_type'	=>'certification',
 				'paged'		=>$paged,
+				'category_name' => 'certification',
 			);
 			
 			$data = get_post_meta( $post->ID, 'blog_posts_pp', true );

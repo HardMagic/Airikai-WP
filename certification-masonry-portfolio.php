037d77737013974429f43415f9e5d2cb5a73ea88
Matt Hackney <matt@themov.org>
2014-10-14 00:03:08 +0000

    Certifications addition

--- certification-masonry-portfolio.php
+++ certification-masonry-portfolio.php
@@ -1,54 +1,76 @@
-<?php 
-/* template name: Certification Masonry Portfolio */
-?>
 <?php
-	$taxonomy = 'portfolio-category';
-	$category_class = ' certifications';
-	$term_ids = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' =>'ids' ));
-	foreach( $term_ids as $term_id){
-		$term = get_term( $term_id, $taxonomy );
-		$category_class .= ' '. $term->slug;	
-	}
-	// get post options data
-	$data = get_post_meta( $post->ID, 'portfolio_options', true );
-	$video_html = isset( $data['video_html'] )?trim( $data['video_html'] ):'';
-	// post content type
-	$p_type = '';
-	$vid_container = '';
-
-	$thumb = dt_get_thumbnail( array(
-		'post_id'	=>$post->ID,
-		'width'		=>240,
-		'upscale'	=>true
-	) );
-			
-	if ( $video_html && !post_password_required() ) {
-		$p_type = 'type-video';
-		$thumb['b_href'] = '#';
-		$vid_container = <<<HEREDOC
-		<div class="highslide-maincontent" data-width="{$data['video_width']}">{$video_html}</div>
-HEREDOC;
-	}
-	
-	$pass_class = post_password_required()?' dt-pass-protected':'';
+/* Template Name: Certification - masonry*/
 ?>
-<div id="<?php echo $post->ID ?>" class="article_box<?php echo $category_class ?> isotope-item <?php echo esc_attr(get_post_time('U', true, $post->ID)); echo $pass_class; ?>">
-	<div class="article_t"></div>
-	<div class="article">
-		<div class="img-holder n-s ro <?php echo $p_type ?>">
-			<a href="<?php the_permalink() ?>" data-img="<?php echo $thumb['b_href'] ?>" title="<?php echo $thumb['caption']; ?>">
-				<img <?php echo $thumb['size'][3] ?> src="<?php echo $thumb['t_href'] ?>" alt="<?php echo $thumb['alt'] ?>"/>
-			</a>
-			<?php echo $vid_container ?>
+<?php get_header() ?>
+<div id="bg">
+	
+	<?php get_template_part('mobile-menu') ?>
+	<div id="top_bg"></div>
+	<div id="holder">
+		<?php get_template_part('aside') ?>
+		<div id="content">
+			<?php if( post_password_required() ): ?>
+			
+			<div class="article_box p">
+				<div class="article_t"></div>
+				<div class="article b">
+					<?php echo get_the_password_form(); ?>
+				</div><!-- .article b end -->
+				<div class="article_b"></div>
+			</div>
+			
+			<?php else: ?>
+			<?php
+			if( !$paged = get_query_var('paged') ){
+				$paged = get_query_var('page');
+			}
+			
+			$args = array(
+				'post_type'	=>'post',
+				'paged'		=>$paged,
+				'category_name' => 'certifications'
+			);
+			
+			$data = get_post_meta( $post->ID, 'blog_posts_pp', true );
+			if ( isset($data['posts_per_page']) && $data['posts_per_page'] ) {
+				$args['posts_per_page'] = $data['posts_per_page'];
+			}
+
+			$temp = $wp_query;
+			$wp_query = new Wp_Query( $args );
+			?>
+			<?php if( $wp_query->have_posts() ): ?>
+			<div class="multicol">
+				<?php while( $wp_query->have_posts() ): $wp_query->the_post(); ?>
+					<?php
+					if( !post_password_required() )
+						get_template_part('certification-masonry', get_post_format());
+					else
+						get_template_part('certification-masonry');
+					?>
+				<?php endwhile ?>
+			</div>
+			<div class="multicol">
+				<?php while( $wp_query->have_posts() ): $wp_query->the_post(); ?>
+					<?php
+					if( !post_password_required() )
+						get_template_part('certification-masonry', get_post_format());
+					else
+						get_template_part('certification-masonry');
+					?>
+				<?php endwhile ?>
+			</div>
+			<div id="nav-above" class="navigation blog">
+				<?php 
+				 if( function_exists('wp_pagenavi') ) wp_pagenavi();
+				 else wp_link_pages();
+				?>
+			</div>
+			<?php else:?>
+			<?php endif ?>
+			<?php $wp_query = $temp; ?>
+			<?php endif;// password protectection ?>
 		</div>
-		<h4 class="entry-title _cf"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
-		<?php the_excerpt() ?>
-		<?php if( current_user_can('edit_posts')): // edit link?>
-			<a href="<?php echo get_edit_post_link($post->ID) ?>" class="button">
-				<span class="but-r"><span><i class="detail"></i><?php echo __( 'Edit', 'dt' ) ?></span></span>
-			</a>
-		<?php endif ?>
-		<a href="<?php the_permalink() ?>" class="button"><span class="but-r"><span><i class="detail"></i><?php _e( 'Details', LANGUAGE_ZONE ) ?></span></span></a>       	
-	</div><!-- .article end -->
-	<div class="article_footer_b"></div>
+	</div>
 </div>
+<?php get_footer() ?>
--- certification-masonry.php
+++ certification-masonry.php
@@ -28,60 +28,9 @@ $t_flag = has_post_thumbnail($post->ID);
 		the_excerpt();
 		wp_link_pages();
 		?>
-		<?php if( current_user_can('edit_posts')): // edit link?>
-			<a href="<?php echo get_edit_post_link($post->ID) ?>" class="button">
-				<span class="but-r"><span><i class="detail"></i><?php echo __( 'Edit', LANGUAGE_ZONE ) ?></span></span>
-			</a>
-		<?php endif ?>
+	
 		<a href="<?php the_permalink() ?>" class="button"><span class="but-r"><span><i class="detail"></i><?php _e( 'Details', LANGUAGE_ZONE) ?></span></span></a>       
 		
-		<?php if( !post_password_required() ): ?>
-		<div class="meta">
-			<div class="ico-l d">   
-				<a href="#" class="ico_link date"></a>
-				<div class="info-block">
-					<span class="grey"><?php _e( 'Published on:', LANGUAGE_ZONE ) ?></span><br/>
-					<a href="<?php the_permalink() ?>"><?php the_time(get_option('date_format'). ' '. get_option('time_format') ) ?></a>
-				</div>
-			</div>           
-			
-			<div class="ico-l">
-				<span class="ico_link author"></span>
-				<div class="info-block">
-					<span class="grey"><?php echo __( 'Author:', LANGUAGE_ZONE ) ?></span><br />                 
-					<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>">
-						<?php echo get_the_author() ?>
-					</a>
-				</div>
-			</div>
-			
-			<?php // category
-			 $categories = get_the_category_list( __( ', ', 'dt' ) );
-			 if( $categories ):
-			?>
-				<div class="ico-l">
-					<span class="ico_link categories"></span>
-					<div class="info-block">
-						<span class="grey"><?php echo __( 'Categories:', 'dt' ) ?></span><br />					
-						<?php echo $categories ?>
-					</div>
-				</div>
-			<?php endif ?>
-			
-			<?php //tags
-			 $tags = get_the_tag_list( '', __( ', ', 'dt' ) );
-			 if( $tags ):			
-			?>
-				<div class="ico-l">
-					<span class="ico_link tags"></span>
-					<div class="info-block">
-						<span class="grey"><?php echo __( 'Tags:', 'dt' ) ?></span><br />                 
-						<?php echo $tags ?>
-					</div>
-				</div>
-			<?php endif ?>
-		</div><!-- meta end -->
-		<?php endif; ?>
 	</div><!-- .article end -->
 	<div class="article_footer_b"></div>
 </div>

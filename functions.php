2feb2814488ea490b5854fb8b1b115cd71be6150
Matt Hackney <matt@themov.org>
2014-12-02 20:04:53 +0000

    Add Widget Box

--- functions.php
+++ functions.php
@@ -103,3 +103,102 @@ function course_division() {
 add_action( 'init', 'course_division' );
 
 add_action( 'init', 'certification_division' );
+
+
+
+/**
+ * Adds a box to the main column on the Post and Page edit screens.
+ */
+function themov_course_id_add_meta_box() {
+
+	$screens = array( 'course' );
+
+	foreach ( $screens as $screen ) {
+
+		add_meta_box(
+			'themov_course_id_sectionid',
+			__( 'Course ID', 'themov_course_id_textdomain' ),
+			'themov_course_id_meta_box_callback',
+			$screen
+		);
+	}
+}
+add_action( 'add_meta_boxes', 'themov_course_id_add_meta_box' );
+
+/**
+ * Prints the box content.
+ * 
+ * @param WP_Post $post The object for the current course.
+ */
+function themov_course_id_meta_box_callback( $post ) {
+
+	// Add an nonce field so we can check for it later.
+	wp_nonce_field( 'themov_course_id_meta_box', 'themov_course_id_meta_box_nonce' );
+
+	/*
+	 * Use get_post_meta() to retrieve an existing value
+	 * from the database and use the value for the form.
+	 */
+	$value = get_post_meta( $post->ID, '_my_meta_value_key', true );
+
+	echo '<label for="themov_course_id">';
+	_e( 'Course ID on TheMOV', 'themov_course_id_textdomain' );
+	echo '</label> ';
+	echo '<input type="text" id="themov_course_id" name="themov_course_id" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+function themov_course_id_save_meta_box_data( $post_id ) {
+
+	/*
+	 * We need to verify this came from our screen and with proper authorization,
+	 * because the save_post action can be triggered at other times.
+	 */
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['themov_course_id_meta_box_nonce'] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['themov_course_id_meta_box_nonce'], 'themov_course_id_meta_box' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	/* OK, it's safe for us to save the data now. */
+	
+	// Make sure that it is set.
+	if ( ! isset( $_POST['themov_course_id_new_field'] ) ) {
+		return;
+	}
+
+	// Sanitize user input.
+	$my_data = sanitize_text_field( $_POST['themov_course_id_new_field'] );
+
+	// Update the meta field in the database.
+	update_post_meta( $post_id, '_my_meta_value_key', $my_data );
+}
+add_action( 'save_post', 'themov_course_id_save_meta_box_data' );
